d64f9cc9f9a2f2736aaf412215c772ebd9d75ef7
Daniel Engbert <d.engbert@student.vu.nl>
2024-03-04 16:43:34 +0100

    download.sh: support PeerRead

--- datasets/download.sh
+++ datasets/download.sh
@@ -7,12 +7,20 @@ SCRIPT_DIR="$(realpath "$(dirname "$0")")"
 function main() {
   cd "$SCRIPT_DIR"
 
-  if ! [ -d MMLU ]; then
+  TARGET="MMLU"
+  if ! [ -d "$TARGET" ]; then
     # MMLU https://github.com/hendrycks/test
     wget https://people.eecs.berkeley.edu/~hendrycks/data.tar
-    tar -xvf data.tar && mv data MMLU && rm -f data.tar
+    tar -xvf data.tar && mv data "$TARGET" && rm -f data.tar
   else
-    echo "MMLU already downloaded!"
+    echo -e "$TARGET already downloaded!\n"
+  fi
+
+  TARGET="PeerRead"
+  if ! [ -d "$TARGET" ]; then
+    git clone git@github.com:allenai/PeerRead.git "$TARGET"
+  else
+    echo -e "$TARGET already downloaded!\n"
   fi
 }
 
