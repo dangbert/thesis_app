5528f37f5c674ad1f506e3be46216b697e2ec04a
Daniel Engbert <d.engbert@student.vu.nl>
2024-03-18 15:43:36 +0100

    frontend: setup theme, create Onboard.tsx (routing not working)

---
+++ frontend/apps/frontend/src/components/Onboard.tsx
@@ -0,0 +1,35 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { CardActions } from '@mui/material';
+
+function Onboard() {
+  const courseName =
+    'Project moderne ontwikkelingen in de farmaceutische wetenschappen';
+  const instructors = ['Danny', 'Fanny'];
+  const message = 'Login with your VU email to join!';
+
+  return (
+    <Card sx={{ maxWidth: 400, m: 'auto', mt: 5 }}>
+      <CardContent>
+        <Typography variant="h5" component="div">
+          You're invited to join a course as a student
+        </Typography>
+        <Typography sx={{ mt: 2 }}>Course: {courseName}</Typography>
+        <Typography>
+          Instructor{instructors.length > 1 ? 's' : ''}:{' '}
+          {instructors.join(', ')}
+        </Typography>
+        <Typography sx={{ mt: 2 }}>{message}</Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="large" variant="contained" sx={{ width: '100%' }}>
+          Join Course
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
+
+export default Onboard;
---
+++ frontend/apps/frontend/src/components/app.tsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+
+import { Route, Routes } from 'react-router-dom';
+import styled from '@emotion/styled';
+
+import { ThemeProvider, Theme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import INITIAL_THEME, {
+  CustomThemeContext,
+  ICustomThemeProviderContext,
+} from './theme';
+import Typography from '@mui/material/Typography';
+import Onboard from './Onboard';
+
+const StyledApp = styled.div`
+  // Your style here
+`;
+
+export function App() {
+  const [theme, setTheme] = useState<Theme>(INITIAL_THEME);
+
+  const customThemeCtx: ICustomThemeProviderContext = {
+    theme: theme,
+    setTheme: (newTheme: Theme) => {
+      console.log('\nsetting new theme:\n', newTheme);
+      setTheme(newTheme);
+    },
+  };
+
+  return (
+    <StyledApp>
+      <CustomThemeContext.Provider value={customThemeCtx}>
+        {/* still using ThemeProvider for the sake of mui's components */}
+        <ThemeProvider theme={customThemeCtx.theme}>
+          <CssBaseline enableColorScheme={true} />
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <Typography variant="h1" gutterBottom>
+                  Welcome Home
+                </Typography>
+              }
+            />
+            <Route path="/join" element={<Onboard />} />
+            <Route
+              path="/test"
+              element={
+                <Typography variant="h2" gutterBottom>
+                  Testing
+                </Typography>
+              }
+            />
+
+            <Route
+              path="*"
+              element={
+                <Typography variant="h1" gutterBottom>
+                  Fallback page
+                </Typography>
+              }
+            />
+          </Routes>
+        </ThemeProvider>
+      </CustomThemeContext.Provider>
+    </StyledApp>
+  );
+}
+
+export default App;
