21a260835f85709fb2d45402358df743a406539b
Daniel Engbert <d.engbert@student.vu.nl>
2024-04-17 13:40:03 +0200

    gemma.py: add instruction following example

--- model/gemma.py
+++ model/gemma.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+################################################################################
+# https://huggingface.co/google/gemma-2b-it
+# code: https://github.com/huggingface/transformers/tree/main/src/transformers/models/gemma
+# format of turns etc: https://ai.google.dev/gemma/docs/formatting
+# https://medium.com/@coldstart_coder/getting-started-with-googles-gemma-llm-using-huggingface-libraries-a0d826c552ae
+################################################################################
 
 import os
 import torch
@@ -17,6 +23,7 @@ MODEL_ID = "google/gemma-2b-it"
 
 def main():
     device = get_device()
+    max_new_tokens = 400
     print(f"using device {device}")
     model, tokenizer = get_model()
 
@@ -24,11 +31,21 @@ def main():
     input_text = "Write me a poem in Dutch."
     input_ids = tokenizer(input_text, return_tensors="pt").to(device)
     with TaskTimer("single generation"):
-        outputs = model.generate(**input_ids, max_new_tokens=400)
+        outputs = model.generate(**input_ids, max_new_tokens=max_new_tokens)
+        print(tokenizer.decode(outputs[0]), "\n")
+
+    # simple instruction example
+    chat = [
+        {"role": "user", "content": "Write me a poem in Dutch with 2 emojis inside"},
+    ]
+    input_ids = tokenizer.apply_chat_template(chat, return_tensors="pt").to(device)
+    with TaskTimer("single instruction generation"):
+        outputs = model.generate(input_ids, max_new_tokens=max_new_tokens)
         print(tokenizer.decode(outputs[0]), "\n")
 
     ### batch generation example
     # (better than using pipeline which abstracts too much away, giving less control)
+    # currenlty it completes the text rather than directly following the instruction
     texts = [
         f"write me a poem in {lang}"
         for lang in ["Dutch", "French", "German", "Spanish"]
@@ -36,7 +53,8 @@ def main():
     texts.append(
         "tell me a short story about a kid at the beach for the first time in Europe"
     )
-    max_new_tokens = 400
+    # chats = [[{ "role": "user", "content": t}] for t in texts]
+
     with TaskTimer(f"batch generation (n={len(texts)})"):
         batch_inputs = tokenizer(texts, return_tensors="pt", padding=True).to(device)
         batch_outputs = model.generate(**batch_inputs, max_new_tokens=max_new_tokens)
